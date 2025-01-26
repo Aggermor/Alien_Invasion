3d92cba710ebaf22a29caa1ce5882655a88e0903
Aggermor <synysterwes@gmail.com>
2025-01-25 18:02:30 -0800

    Built the main class for the game to run. I hard coded some values which will be refactored to a settings file later

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Alien_Invasion
+#Alien_Invasion  
+  
+I installed pygame  
+`python -m pip install pygame`  
+I did need to create a venv in VSCode because I have multiple versions of python or something I don't understand.
+  
+- in pygame a surface is a part of the screen that you can draw on  
+- an event is an action that the user does like pressing a key or moving the mouse  
+- an event loop is a loop that listens for events and responds to them  
+    - The speed of the game is controlled by the speed of the loop.   
+    - to control a consistent frame rate, you can use the clock object in pygame
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,39 @@
-# Python Crash Course Part II: Project 1
+# Python Crash Course (Third Edition) - Part II: Project 1
 # Alien Invasion Game 
 
-import sys
-import pygame
+import sys      # sys module allows us to exit the game when the player quits
+import pygame   # pygame module contains the functionality needed to make a game
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock() # Create a clock object to control the speed of the game
+
+        self.screen = pygame.display.set_mode((1200, 800)) # Set the display dimensions
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230) # light grey RGB color value (0 through 255)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60) # Set the frame rate to 60 frames per second
+
+if __name__ == '__main__': # This line checks whether the file is being run directly
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
